2c4cb1ab786a053fb589c456012fa6cca8247f25
vadlamudi pradeep <vadlamudipradeep2000>
2022-10-10 14:16:10 +0530

    i know git

--- 2.js
+++ 2.js
@@ -1,2 +1,6 @@
 console.log('second file');
-console.log('jkbhkghjghjh');
+console.log('jkbhkghjghjh');let x = document.getElementsByClassName('list-group-item');
+for(let i=0;i<x.length;i++){
+    x[2].style.backgroundColor='green';
+    x[i].style.fontWeight  = 'bold';
+}
---
+++ 3.js
@@ -0,0 +1,5 @@
+let x = document.getElementsByClassName('list-group-item');
+for(let i=0;i<x.length;i++){
+    x[2].style.backgroundColor='green';
+    x[i].style.fontWeight  = 'bold';
+}
