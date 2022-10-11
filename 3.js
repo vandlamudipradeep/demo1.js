3fcb4e2e07e7c75805184f8e0ea9e3807ceeb359
vadlamudi pradeep <vadlamudipradeep2000>
2022-10-11 08:29:08 +0530

    I Love Git

--- 3.js
+++ 3.js
@@ -22,52 +22,50 @@
 
 
 
-let x = document.getElementsByClassName('list-group-item');
-for(let i=0;i<x.length;i++){
-    x[2].style.backgroundColor='green';
-    x[i].style.fontWeight  = 'bold';
-}
+// let x = document.getElementsByClassName('list-group-item');
+// for(let i=0;i<x.length;i++){
+//     x[2].style.backgroundColor='green';
+//     x[i].style.fontWeight  = 'bold';
+// }
 
-let y = document.getElementsByClassName('list-group-item');
-for(let j = 0 ;j<y.length ;j++){
-    y[j].style.backgroundColor='red';
-}
+// let y = document.getElementsByClassName('list-group-item');
+// for(let j = 0 ;j<y.length ;j++){
+//     y[j].style.backgroundColor='red';
+// }
 
-let z = document.getElementsByTagName('li');
-for(let a = 0 ; a<z.length ;a++){
-    z[a].style.backgroundColor='yellow ';
-    z[a].style.fontStyle = 'italic';
-    z[a].style.borderBottom = 'solid  black';
-    z[a].style.borderTop = 'solid red';
-    z[a].style.borderLeft = 'solid blue';
-    z[a].style.alignSelf='left'
-    z[a].style.borderRight = 'solid grey';
-    // z[a].style.borderRadius ='60%';
-    z[a].style.borderBlockEndColor='violet';
-}
-let input = document.querySelector('input');
-input.value = "Hello";
-let submit = document.querySelector('input[type="submit"]');
-submit.value="SEND";
+// let z = document.getElementsByTagName('li');
+// for(let a = 0 ; a<z.length ;a++){
+//     z[a].style.backgroundColor='yellow ';
+//     z[a].style.fontStyle = 'italic';
+//     z[a].style.borderBottom = 'solid  black';
+//     z[a].style.borderTop = 'solid red';
+//     z[a].style.borderLeft = 'solid blue';
+//     z[a].style.alignSelf='left'
+//     z[a].style.borderRight = 'solid grey';
+//     // z[a].style.borderRadius ='60%';
+//     z[a].style.borderBlockEndColor='violet';
+// }
+// let input = document.querySelector('input');
+// input.value = "Hello";
+// let submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND";
 
-let item = document.querySelector('.list-group-item');
-item.style.color = 'red';
-let lastitem1 = document.querySelector('.list-group-item:last-child');
-lastitem1.style.color = 'blue';
-let seconditem = document.querySelectorAll('.list-group-item');
-seconditem[1].style.backgroundColor ='green';
+// let item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+// let lastitem1 = document.querySelector('.list-group-item:last-child');
+// lastitem1.style.color = 'blue';
+// let seconditem = document.querySelectorAll('.list-group-item');
+// seconditem[1].style.backgroundColor ='green';
 
-let invisible = document.querySelectorAll('.list-group-item');
-invisible[2].style.display = 'none';
+// let invisible = document.querySelectorAll('.list-group-item');
+// invisible[2].style.display = 'none';
 
-
-
-let odd = document.querySelectorAll('li:nth-child(odd)');
- let even = document.querySelectorAll('li:nth-child(even)');
- for(let i= 0 ;i<even.length ;i++){
-  odd[i].style.backgroundColor='green';
+// let odd = document.querySelectorAll('li:nth-child(odd)');
+//  let even = document.querySelectorAll('li:nth-child(even)');
+//  for(let i= 0 ;i<even.length ;i++){
+//   odd[i].style.backgroundColor='green';
     
-}
+// }
 
 
 
@@ -79,3 +77,116 @@ let odd = document.querySelectorAll('li:nth-child(odd)');
 //       x[i].style.backgroundColor='green';
 //     }
 //  }
+
+
+// parentElement
+// lastelementchild
+// lastchild
+// createchild
+// firstelementchild
+// firstchild
+// nextsibling
+// nextelementsibling
+// previoussibling
+// previouselementsibling
+// createelement
+// setAttribute
+// createtesxtnode
+// appendchild
+
+
+// Now go head and add HEllo word before Item Lister
+// Now go head and add HEllo word before Item 1
+// If stuck for long check the hints
+
+
+//Traversing The  DOM//
+
+let itemList = document.querySelector('#items');
+//parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor='grey';
+// console.log(itemList.parentNode.parentNode);
+
+//parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.parentElement.parentElement.style.backgroundColor = 'red';
+// itemList.parentElement.parentElement.style.fontStyle = 'italic';
+// itemList.parentElement.style.fontWeigth = 'bold';
+
+//childNodes 
+// console.log(itemList.childNodes);
+// itemList.childNodes.style.Color = 'blue';
+
+//childern
+
+// console.log(itemList.children);
+// console.log(itemList.children[3]);
+// itemList.children[2].style.backgroundColor='yellow';
+
+//firstchild 
+// console.log(itemList.firstChild);
+
+//firstElementchild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'HELLO';
+
+//lastChild
+// console.log(itemList.lastChild);
+// itemList.lastChild.textContent = 'reddy';
+// //lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'pardeep';
+
+
+//nextSibling
+// console.log(itemList.nextSibling);
+
+// //nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+//previousibling
+// console.log(itemList.previousSibling);
+//previousElementsibiling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'red';
+
+// createElement
+//create div 
+
+let newDiv = document.createElement('div');
+//Add Class
+newDiv.className='hello';
+//Add id 
+newDiv.id = 'hello1';
+//Add attr
+newDiv.setAttribute('title', 'Hello Div');
+//create text node
+let newDivText = document.createTextNode('HEllo');
+//Add text to div 
+newDiv.appendChild(newDivText);
+let container = document.querySelector('header .container');
+let h1 = document.querySelector('header h1');
+console.log(newDiv);
+container.insertBefore(newDiv , h1);
+
+let newLi = document.createElement('li');
+newLi.className = 'list-group-item 0';
+let newLitext = document.createTextNode('HEllo');
+newLi.appendChild(newLitext);
+let list = document.querySelector('ul');
+let li = document.querySelector('ul li');
+list.insertBefore(newLi,li);
+
+
+
+
+
+
+
+
+
+
+
+
+
